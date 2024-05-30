b1307a748f8d7e69bfd51f22750cc3e9a6cbf2c1
DESKTOP-V4BAF83\fira <fajriarfid@gmail.com>
2024-05-30 21:19:09 +0700

    3. tugas form

---
+++ src/Tugas/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Tugasform from "./Tugas";
+
+export default class Tugas extends React.Component{
+    render(){
+        return(
+
+            <div>
+                <Tugasform/>
+            </div>
+        )
+            
+        
+    }
+}
